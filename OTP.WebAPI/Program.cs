f83ea36fc3e60694f086cdf22070193015778f30
Atefesaki1376 <atefesaki5@gmail.com>
2025-08-26 20:46:06 +0330

    changed

--- OTP.WebAPI/Program.cs
+++ OTP.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.OpenApi.Models;
 using OTP.WebAPI.Interfaces;
 using OTP.WebAPI.Services;
 using Scalar.AspNetCore;
+using Serilog;
 
 namespace OTP.WebAPI
 {
@@ -10,6 +11,12 @@ namespace OTP.WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Configure Serilog
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .WriteTo.Console()
+                .WriteTo.File("logs/otp-api.log", rollingInterval: RollingInterval.Day));
+
             builder.WebHost.ConfigureKestrel(options =>
             {
                 options.ListenLocalhost(5000); // http://localhost:5000
